4e49d50ec1ab6c84381a8c956503cd693f8fa6e8
Ubuntu <ubuntu@ip-172-31-45-194.eu-west-1.compute.internal>
2021-09-16 22:07:48 +0000

    update api filter

--- image_mongo_flaskapi/api.py
+++ image_mongo_flaskapi/api.py
@@ -44,6 +44,18 @@ def collection():
     col = db['wine_test']
     return  make_response(jsonify({"collection_names": db.list_collection_names()}), 200)
 
+@api.route('/mongodb/wine/wine_test/find_one', methods=['GET']) #renvoie un document de la collection wine_test
+def find_one():
+    client = MongoClient(
+        host='mongodb',
+        port=27017,
+        username='data_user',
+        password='data_pwd',
+        authSource='wine'
+    )
+    db = client['wine']
+    col = db['wine_test']
+    return  make_response(jsonify({"document": str(col.find_one())}), 200)
 
 @api.route('/mongodb/wine/wine_test/distinct', methods=['GET']) #renvoie les valeurs uniques de la clé sélectionnée
 def distinct():
@@ -56,11 +68,37 @@ def distinct():
     )
     db = client['wine']
     col = db['wine_test']
-#    key = request.args.get('key')
-#    return col.find(filter={key:{'$exists': True}}).distinct(key)
-#    return  make_response(jsonify({"document": col.find_one()}), 200)
-#    return  make_response(print(col.find_one()), 200)
-    return  make_response(col.find_one(), 200)
+    key = request.args.get('key')
+    return make_response(jsonify({key: str(col.find(filter={key:{'$exists': True}}).distinct(key))}),200)
+
+@api.route('/mongodb/wine/wine_test/filter', methods=['GET']) #renvoie les documents correspondants aux critères sélectionnés
+def filter():
+    client = MongoClient(
+        host='mongodb',
+        port=27017,
+        username='data_user',
+        password='data_pwd',
+        authSource='wine'
+    )
+    db = client['wine']
+    col = db['wine_test']
+    country = request.args.get('country', default = '*', type = str)
+    points = request.args.get('points', default = '*', type = str)
+#    taster_name = request.args.get('taster_name', default = '*', type = str)
+#    price = request.args.get('price', default = '*', type = str)
+#    designation = request.args.get('designation', default = '*', type = str)
+#    variety = request.args.get('variety', default = '*', type = str)
+#    region_1 = request.args.get('region_1', default = '*', type = str)
+#    region_2 = request.args.get('region_2', default = '*', type = str)
+    province = request.args.get('province', default = '*', type = str)
+#    winery = request.args.get('winery', default = {'$exists': True}, type = str)
+    results = list(col.find(filter={'country':country,'points':points, 'province':province}))
+#    filtered_query = []
+    return make_response(jsonify({'query length':len(results)}),200)
+#    for result in results:
+#        filtered_query.append(str(result))
+#    return make_response(jsonify({'filtered_query': str(filtered_query)}),200)
+
 
 if __name__ == '__main__':
     api.run(host="0.0.0.0", port=5000)
